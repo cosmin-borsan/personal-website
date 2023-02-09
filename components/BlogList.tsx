bbb3db0c0b97c9d2ecc695fc4fa3805629686978
Cosmin Borsan <cosmin.borsan@icloud.com>
2023-02-09 18:04:03 +0200

    single quote settings

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import urlFor from "../lib/urlFor";
-import ClientSideRoute from "./ClientSideRoute";
+import Image from 'next/image';
+import urlFor from '../lib/urlFor';
+import ClientSideRoute from './ClientSideRoute';
 
 type Props = {
     posts: Post[];
@@ -9,42 +9,42 @@ type Props = {
 function BlogList({ posts }: Props) {
     return (
         <div>
-            <hr className="border-[#F7AB0A] mb-10" />
+            <hr className='border-[#F7AB0A] mb-10' />
             <div>
                 {/* Posts */}
                 {posts.map((post) => (
                     <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
-                        <div className="flex flex-col group cursor-pointer">
-                            <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
+                        <div className='flex flex-col group cursor-pointer'>
+                            <div className='relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out'>
                                 <Image
-                                    className="object-cover object-left lg:object-center"
+                                    className='object-cover object-left lg:object-center'
                                     src={urlFor(post.mainImage).url()}
                                     alt={post.author.name}
                                     fill
                                 />
-                                <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between">
+                                <div className='absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between'>
                                     <div>
-                                        <p className="font-bold">{post.title}</p>
+                                        <p className='font-bold'>{post.title}</p>
                                         <p>
-                                            {new Date(post._createdAt).toLocaleDateString("en-US", {
-                                                day: "numeric",
-                                                month: "long",
-                                                year: "numeric",
+                                            {new Date(post._createdAt).toLocaleDateString('en-US', {
+                                                day: 'numeric',
+                                                month: 'long',
+                                                year: 'numeric',
                                             })}
                                         </p>
                                     </div>
-                                    <div className="flex felx-col md:flex-row gap-y-2 md: gap-x-2 items-center">
+                                    <div className='flex felx-col md:flex-row gap-y-2 md: gap-x-2 items-center'>
                                         {post.categories.map((category) => (
-                                            <div className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
+                                            <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold'>
                                                 {category.title}
                                             </div>
                                         ))}
                                     </div>
                                 </div>
                             </div>
-                            <div className="mt-5 flex-1">
-                                <p className="underline text-lg font-bold">{post.title}</p>
-                                <p className="text-gray-500">{post.description}</p>
+                            <div className='mt-5 flex-1'>
+                                <p className='underline text-lg font-bold'>{post.title}</p>
+                                <p className='text-gray-500'>{post.description}</p>
                             </div>
                         </div>
                     </ClientSideRoute>
